49b0b812600da34bb9739427720e994c850146cf
Marcus Linn Miller Jr <marcusmiller220@gmail.com>
2017-05-10 15:49:51 -0400

    first commit

--- js/compliments/compliments.controller.js
+++ js/compliments/compliments.controller.js
@@ -1,11 +1,13 @@
 `use strict`;
 
 (function(){
-  var app = angular.module("compliments");
-  app.controller("complimentsController", ComplimentsController);
+  var app = angular.module("angularcomp");
+  app.controller("mainController", mainController);
 
-  function ComplimentsController(){
+  function mainController(){
     var vm = this;
+    var randomIndex = Math.floor(Math.random() * data.length)
++    vm.data = data[randomIndex];
   }
 
 }());
